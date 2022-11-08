a98ca0c1058086b92b28a0889d1f00ea5cf575bd
Neto002 <antoniogfneto92@hotmail.com>
2022-11-08 10:46:48 -0300

    greeting controller

---
+++ 05_FirstSteps/spring-rest-api-udemy/src/main/java/com/neto/curso/SpringRestApiUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.neto.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiUdemyApplication.class, args);
+    }
+
+}
