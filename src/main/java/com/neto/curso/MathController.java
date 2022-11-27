5dc7692aeeb7b8b480eb351dc1c7ca681413e510
Neto002 <antoniogfneto92@hotmail.com>
2022-11-27 16:13:21 -0300

    tratando numeros

--- src/main/java/com/neto/curso/MathController.java
+++ src/main/java/com/neto/curso/MathController.java
@@ -26,10 +26,27 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
-        return 1D;
+
+        if (strNumber == null) {
+            return 0D;
+        }
+
+        String number = strNumber.replaceAll(",", ".");
+
+        if (isNumeric(number)) {
+            return Double.parseDouble(number);
+        }
+
+        return 0D;
     }
 
     private boolean isNumeric(String strNumber) {
-        return true;
+        if (strNumber == null) {
+            return false;
+        }
+
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
